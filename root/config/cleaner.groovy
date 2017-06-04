a70f22d86d8bd3a545391253c05ff67bad1a3150
RiX <rix1337@users.noreply.github.com>
2017-06-04 16:35:10 +0200

    Update cleaner.groovy

--- root/config/cleaner.groovy
+++ root/config/cleaner.groovy
@@ -1,45 +1,52 @@
-// filebot -script fn:cleaner [--action test] /path/to/media/
-def deleteRootFolder = tryQuietly{ root.toBoolean() }
+#!/usr/bin/env filebot -script
+
+
+def deleteRootFolder = any{ root.toBoolean() }{ false }
+
+def ignore  = any{ ignore }{ /extrathumbs/ }
+def exts    = any{ exts }{ /jpg|jpeg|png|gif|ico|nfo|info|xml|htm|html|log|m3u|cue|srt|sub|idx|smi|sup|md5|sfv|txt|rtf|url|db|dna|log|tgmd|json|data|ignore|srv|srr|nzb|vbs|ini/ }
+def terms   = any{ terms }{ /sample|trailer|extras|deleted.scenes|music.video|scrapbook|DS_Store/ }
+def minsize = any{ minsize.toLong() }{ 20 * 1024 * 1024 }
+def maxsize = any{ maxsize.toLong() }{ 100 * 1024 * 1024 }
+
+
+def testRun = _args.action.equalsIgnoreCase('test')
+
 
 /*
  * Delete orphaned "clutter" files like nfo, jpg, etc and sample files
  */
-def isClutter(f) {
-	// white list
-	def ignore  = tryQuietly{ ignore }          ?: /extrathumbs/
-	if (f.path =~ "(?i)\\b($ignore)\\b") return false
-	
-	// black list
-	def exts    = tryQuietly{ exts }            ?: /jpg|jpeg|png|gif|nfo|xml|htm|html|log|srt|sub|idx|md5|sfv|txt|rtf|url|db|dna|log|tgmd|json|data|srv|srr|nzb|par\d+|part\d+/
-	def terms   = tryQuietly{ terms }           ?: /sample|trailer|extras|deleted.scenes|music.video|scrapbook|DS_Store/
-	def minsize = tryQuietly{ minsize as Long } ?:  20 * 1024 * 1024
-	def maxsize = tryQuietly{ maxsize as Long } ?: 100 * 1024 * 1024
-	
+def isClutter = { f ->
+	// whitelist
+	if (f.path.findMatch(ignore))
+		return false
+
 	// file is either too small to have meaning, or to large to be considered clutter
 	def fsize = f.length()
 
 	// path contains blacklisted terms or extension is blacklisted
-	if (f.extension ==~ "(?i)($exts)" && fsize < maxsize)
+	if (f.extension ==~ exts && fsize < maxsize)
 		return true
 
-	if (f.path =~ "(?i)\\b($terms)\\b" && fsize < maxsize)
+	if (f.path.findMatch(/\b(/ + terms + /)\b/) && fsize < maxsize)
 		return true
 
-	if ((f.isVideo() || f.isAudio()) && fsize < minsize)
+	// NOTE: some smb filesystem implementations are buggy and known to incorrectly return filesize 0 for all files
+	if (f.isVideo() && fsize < minsize && fsize > 0)
 		return true
 
 	return false
 }
 
 
-def clean(f) {
-	println "Delete $f"
-	
+def clean = { f ->
+	log.info "Delete $f"
+
 	// do a dry run via --action test
-	if (_args.action == 'test') {
+	if (testRun) {
 		return false
 	}
-	
+
 	return f.isDirectory() ? f.deleteDir() : f.delete()
 }
 
@@ -48,7 +55,12 @@ def clean(f) {
 def hasMediaFiles = { dir -> dir.isDirectory() && dir.getFiles().find{ (it.isVideo() || it.isAudio()) && !isClutter(it) } }.memoize()
 
 // delete clutter files in orphaned media folders
-args.getFiles{ isClutter(it) && !hasMediaFiles(it.dir) }.each { clean(it) }
+args.getFiles{ isClutter(it) && !hasMediaFiles(it.dir) }.each{ clean(it) }
 
 // delete empty folders but exclude given args
-args.getFolders().sort().reverse().each { if (it.isDirectory() && it.listFiles()?.length == 0) { if (deleteRootFolder || !args.contains(it)) clean(it) } }
+args.getFolders().sort().reverse().each{
+	if (it.isDirectory() && !it.hasFile{ it.isDirectory() || !isClutter(it) }) {
+		if (deleteRootFolder || !args.contains(it))
+			clean(it)
+	}
+}
